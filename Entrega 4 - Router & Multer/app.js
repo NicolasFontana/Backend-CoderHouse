20085f3272c7c106b8c9f8c58f9e15aa14559b5d
NicolasFontana <nico.fontana12@gmail.com>
2022-08-10 22:36:47 -0300

    Entrega 5: motores de plantillas

--- Entrega 4 - Router & Multer/app.js
+++ Entrega 4 - Router & Multer/app.js
@@ -2,26 +2,29 @@ const express = require("express");
 const Contenedor = require("./class/contenedor");
 const { Router } = express;
 
+const PORT = 8000
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 const router = Router();
 const productos = new Contenedor(__dirname + "/data/productos.json");
 
 app.use("/api/productos", router);
-app.use(express.static("./views"));
-app.listen(8080);
+// app.use(express.static("./views"));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
 
 router.get("/", (req, res) => {
     return res.json(productos.content);
   });
-  
+
   router.get("/:id", (req, res) => {
     let id = Number(req.params.id);
     return res.json(productos.getById(id));
   });
-  
+
   router.post("/", (req, res) => {
     let obj = req.body;
     return res.json(productos.save(obj));
